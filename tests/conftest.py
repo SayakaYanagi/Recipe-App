6423d715c6487fb5f0201131baf7820a20ca4679
Sayaka.Yanagi <sayanagijp@gmail.com>
2025-09-07 10:22:55 +0100

    Add 0

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,4 +3,4 @@ import sys
 import os
 
 # Add the project root to sys.path so "import utils" works
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
